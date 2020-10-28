9bf0f0cf73fe79ee1933d8e5c0a2a12caa175f74
Mohan Kumar <mks10685@gmail.com>
2020-10-28 16:37:43 +0530

    first layout

---
+++ index.js
@@ -0,0 +1,92 @@
+let songsImage = document.getElementById("songsImage");
+let songsName = document.getElementById("song-name");
+let songSlider = document.getElementById("seekSlider");
+let currentTime = document.getElementById("time-spent");
+let duration = document.getElementById("song-duration");
+let playOrPaused = document.getElementById("playOrPause");
+let volumeSlider = document.getElementById("volume");
+let volumeIcon = document.getElementById("volumeIcon");
+let likeIcon = document.getElementById("like");
+let musicPlayer = document.getElementById("music-player");
+
+let state = true;
+
+let song = new Audio();
+let currentSongIndex = 0;
+window.onload = playSongs;
+
+function playSongs(){
+    song.src = songsDetails[currentSongIndex].audioSrc;
+    songsImage.src = songsDetails[currentSongIndex].imageSrc;
+    songsName.textContent = songsDetails[currentSongIndex].title;
+    song.play();
+    setTimeout(showDuration, 500);
+}
+setInterval(updateSongSlider, 1000);
+
+function updateSongSlider(){
+    let c = Math.round(song.currentTime);
+    songSlider.value = c;
+    currentTime.textContent = convertTime(c).min + ":" + convertTime(c).sec;
+    if(song.ended){
+        next();
+    }
+}
+function convertTime(secs){
+    let min = Math.floor(secs/60);
+    let sec = secs % 60;
+    min = (min < 10) ? "0"+min: min; 
+    sec = (sec < 10)? "0"+sec: sec;
+    return {min, sec};
+}
+function showDuration(){
+    let d = Math.floor(song.duration);
+    songSlider.setAttribute("max", d);
+    duration.textContent = convertTime(d).min + ":" + convertTime(d).sec;
+}
+function playOrPause(){
+    if(song.paused){
+        song.play();
+        playOrPaused.className = "fas fa-pause";
+    }else{
+        song.pause();
+        playOrPaused.className = "fas fa-play";
+    }
+}
+
+function next(){
+    currentSongIndex++; 
+    currentSongIndex = (currentSongIndex > songsDetails.length-1) ? 0 : currentSongIndex;
+    playSongs();
+}
+
+function previous(){
+    currentSongIndex--;
+    currentSongIndex = (currentSongIndex < 0) ? songsDetails.length-1: currentSongIndex;
+    playSongs();
+}
+function seekSong(){
+    song.currentTime = songSlider.value;
+    currentTime.textContent = convertTime(song.currentTime).min + ":" + convertTime(song.currentTime).sec;
+    console.log("its works");
+}
+
+function adjustVolume(){
+    song.volume = volumeSlider.value/100;
+    if(song.volume == 0) {
+        volumeIcon.className = "fas fa-volume-mute";
+    }else{
+        volumeIcon.className = "fas fa-volume-up";
+    }
+}
+function like(){
+    if(state){
+        likeIcon.className = "fas fa-heart";
+        likeIcon.style.fill = "red";
+        console.log(likeIcon.style.fill);
+        state = false; 
+    }else{
+        likeIcon.className = "far fa-heart";
+        state = true;
+    }
+}
---
+++ songs.js
@@ -0,0 +1,62 @@
+const songsDetails = [
+    {
+        id:1,
+        title: "The Chainsmokers - Closer ft. Halsey",
+        imageSrc: "songsImage/closer.jpg",
+        audioSrc: "songs/The Chainsmokers - Closer ft. Halsey.mp3",
+    },
+    {
+        id:2,
+        title:"Doja Cat - Say So Why dont you say so ",
+        imageSrc:"songsImage/Say_So.png",
+        audioSrc: "songs/Doja Cat - Say So.mp3",
+    },
+    {
+        id:3,
+        title:"Camila Cabello ‒ Havana ft. Young Thug",
+        imageSrc:"songsImage/Havana.png",
+        audioSrc: "songs/Camila Cabello ‒ Havana 🎤 ft. Young Thug.mp3",
+    },
+    {
+        id:4,
+        title: "Don't Let Me Down- The Chainsmokers ft. Daya",
+        imageSrc:"songsImage/Don't_Let_Me_Down.png",
+        audioSrc: "songs/Don't Let Me Down- The Chainsmokers ft. Daya.mp3",
+    },
+    {
+        id:5,
+        title:"Marshmello & Anne-Marie - FRIENDS",
+        imageSrc: "songsImage/Friends.jpg",
+        audioSrc: "songs/Marshmello & Anne-Marie - FRIENDS.mp3",
+    },
+    {
+        id:6,
+        title:"Brayan Adams Summer Of 69",
+        imageSrc:"songsImage/Summer_of_'69.jpg",
+        audioSrc: "songs/Brayan Adams Summer Of 69.mp3",
+    },
+    {
+        id:7,
+        title:"Shawn Mendes - Treat You Better",
+        imageSrc:"songsImage/Treat_You_Better.png",
+        audioSrc: "songs/Shawn Mendes - Treat You Better.mp3",
+    },
+    {
+        id:8,
+        title: "Ed Sheeran - Shape Of You",
+        imageSrc:"songsImage/Shape_Of_You.png",
+        audioSrc: "songs/Ed Sheeran - Shape Of You.mp3",
+    },
+    {
+        id:9,
+        title: "G-Eazy & Halsey - Him & I",
+        imageSrc:"songsImage/Him_ and _I 1.png",
+        audioSrc: "songs/G-Eazy & Halsey - Him & I.mp3",
+    },
+    {
+        id:10,
+        title: "Perfect - Ed Sheeran",
+        imageSrc:"songsImage/perfect.jpg",
+        audioSrc: "songs/Perfect - Ed Sheeran.mp3",
+    },
+]
